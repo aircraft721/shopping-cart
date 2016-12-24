47b08a1bdab20dca5f4d5ce3c68a7dbbf5f54828
Dan Constantinescu <dan_constantinescu@outlook.com>
2016-12-24 23:41:23 +0200

    index view product

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,10 +5,20 @@
     <title>@yield('title')</title>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="{{ URL::to('src/css/app.css') }}">
     @yield('styles')
 </head>
 <body>
-@yield('content')
+@include('partials.header')
+
+<div class="container">
+    @yield('content')
+</div>
+
+
 
 <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
 
--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -4,6 +4,95 @@
 
 @section('content')
 
-    <h1>It workssss!!</h1>
+    <div class="row">
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="http://hyperdub.s3.amazonaws.com/images/large/HDBCD002.png" alt="..." class="img-responsive">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab doloribus hic incidunt nulla quam quasi saepe vero, voluptas voluptatem voluptates. Animi blanditiis dignissimos doloremque, ea eos ipsum mollitia nostrum voluptates!</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">$12</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="http://hyperdub.s3.amazonaws.com/images/large/HDBCD002.png" alt="..." class="img-responsive">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab doloribus hic incidunt nulla quam quasi saepe vero, voluptas voluptatem voluptates. Animi blanditiis dignissimos doloremque, ea eos ipsum mollitia nostrum voluptates!</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">$12</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="http://hyperdub.s3.amazonaws.com/images/large/HDBCD002.png" alt="..." class="img-responsive">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab doloribus hic incidunt nulla quam quasi saepe vero, voluptas voluptatem voluptates. Animi blanditiis dignissimos doloremque, ea eos ipsum mollitia nostrum voluptates!</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">$12</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+    <div class="row">
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="http://hyperdub.s3.amazonaws.com/images/large/HDBCD002.png" alt="..." class="img-responsive">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab doloribus hic incidunt nulla quam quasi saepe vero, voluptas voluptatem voluptates. Animi blanditiis dignissimos doloremque, ea eos ipsum mollitia nostrum voluptates!</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">$12</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="http://hyperdub.s3.amazonaws.com/images/large/HDBCD002.png" alt="..." class="img-responsive">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab doloribus hic incidunt nulla quam quasi saepe vero, voluptas voluptatem voluptates. Animi blanditiis dignissimos doloremque, ea eos ipsum mollitia nostrum voluptates!</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">$12</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="http://hyperdub.s3.amazonaws.com/images/large/HDBCD002.png" alt="..." class="img-responsive">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab doloribus hic incidunt nulla quam quasi saepe vero, voluptas voluptatem voluptates. Animi blanditiis dignissimos doloremque, ea eos ipsum mollitia nostrum voluptates!</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">$12</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
 @stop
 
