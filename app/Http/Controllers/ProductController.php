c2ac4d910255de156055b8e91ac845550da7f537
Dan Constantinescu <dan_constantinescu@outlook.com>
2016-12-25 18:32:44 +0200

    Shopping cart logic and sessions

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,11 +6,23 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Product;
+use Session;
+use App\Cart;
 
 class ProductController extends Controller
 {
     public function getIndex(){
         $products = Product::all();
         return view('shop.index')->withProducts($products);
+    }
+
+    public function getAddToCart(Request $request, $id) {
+        $product = Product::find($id);
+        $oldCart = Session::has('cart') ? Session::get('cart') : null;
+        $cart = new Cart($oldCart);
+        $cart->add($product, $product->id);
+
+        $request->session()->put('cart',$cart);
+        return redirect()->route('product.index');
     }
 }
